429c2d91cc1ceaceb13d1a1fe177221d10f68484
reusmana <reusmanasujani@gmail.com>
2025-10-22 10:44:29 +0700

    fixing:new calnedar

--- javascript/editor.js
+++ javascript/editor.js
@@ -1,5 +1,1 @@
-﻿var pageEditor = {
-    "setting": {},
-    "pageAnnos": [[]]
-};
-//"pageAnnos": [[]] 如果这里不是这样的结构会影响到语音助手的设置
+var pageEditor = {"setting":{},"pageAnnos":[[],[],[],[],[],[],[],[],[],[],[],[]]};
